d3d8852d43f1b3dc3b88d61d156af4cff3d87ea2
Anand Chowdhary <mail@anandchowdhary.com>
2020-03-18 23:29:08 +0530

    :recycle: Use temp dir to exec

--- index.ts
+++ index.ts
@@ -6,6 +6,7 @@ import fs from "fs";
 import mkdirp from "mkdirp";
 import { join, resolve } from "path";
 import { URL } from "url";
+import { tmpdir } from "os";
 import { cd, exec, rm } from "shelljs";
 import modules from "builtin-modules";
 
@@ -15,13 +16,11 @@ if (!url) throw new Error("No URL found");
 const parsed = new URL(url);
 if (!parsed.protocol) throw new Error("Invalid URL");
 
-const rootDir = join("temp");
-
-mkdirp(rootDir).then(() => {
-  const file = fs.createWriteStream(join(rootDir, "script.js"));
-  const request = (parsed.protocol === "https:" ? https : http).get(
-    url,
-    response => {
+fs.mkdtemp(join(tmpdir(), "foo-"), (error, rootDir) => {
+  if (error) throw error;
+  mkdirp(rootDir).then(() => {
+    const file = fs.createWriteStream(join(rootDir, "script.js"));
+    (parsed.protocol === "https:" ? https : http).get(url, response => {
       response.pipe(file);
       response.on("end", () => {
         const jsFile = fs.readFileSync(join(rootDir, "script.js"), {
@@ -44,11 +43,15 @@ mkdirp(rootDir).then(() => {
           })
           .filter(i => !modules.includes(i));
         cd(resolve(rootDir));
-        exec(`npm install ${dependencies.join(" ")}`);
+        exec(
+          `npm install ${dependencies.join(
+            " "
+          )} --silent --quiet --no-progress --loglevel=silent`
+        );
         exec("node script.js");
         exec("cd ..");
         rm("-rf", rootDir);
       });
-    }
-  );
+    });
+  });
 });
