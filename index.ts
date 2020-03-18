c357b56d8894c4d31dbd0b2e13dab7dc797bb242
Anand Chowdhary <mail@anandchowdhary.com>
2020-03-18 22:51:13 +0530

    :heavy_plus_sign: Add mkdirp

--- index.ts
+++ index.ts
@@ -1,3 +1,23 @@
 #!/usr/bin/env node
 
-console.log(1);
+import https from "https";
+import http from "http";
+import fs from "fs";
+import mkdirp from "mkdirp";
+import { join } from "path";
+import { URL } from "url";
+
+const url = process.argv[2];
+if (!url) throw new Error("No URL found");
+
+const parsed = new URL(url);
+if (!parsed.protocol) throw new Error("Invalid URL");
+
+const rootDir = join(".cache");
+
+const file = fs.createWriteStream(join(rootDir, "script.js"));
+const request = (parsed.protocol === "https:" ? https : http).get(url, function(
+  response
+) {
+  response.pipe(file);
+});
